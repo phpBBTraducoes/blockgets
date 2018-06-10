509a6d1be8582e27cbbd1ad54c50822a59076b03
eunaumtenhoid <eunaumtenhoid@outlook.com>
2018-06-10 20:38:23 -0300

    Update

--- language/pt_br/chat_addon.php
+++ language/pt_br/chat_addon.php
@@ -1,12 +1,12 @@
 <?php
 /** 
-* acp_blockgets.php [Brazilian Portuguese [pt_br]]
-* Brazilian Portuguese  translation by eunaumtenhoid (c) 2017 [ver 2.0.0] (https://github.com/phpBBTraducoes)
+* chat_addon.php [Brazilian Portuguese [pt_br]]
+* Brazilian Portuguese  translation by eunaumtenhoid (c) 2018 [ver 2.0.1] (https://github.com/phpBBTraducoes)
 * @package language cBB Blockgets
-* @version v2.0.0 30/07/2017
+* @version v2.0.1 10/06/2018
 *
-* @copyright (c) 2017 CaniDev
-* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* @copyright (c) 2018 CaniDev
+* @license https://opensource.org/licenses/gpl-license.php GNU Public License
 */
 
 // @ignore
@@ -23,4 +23,10 @@ if(empty($lang['blocks']))
 $lang['blocks'] = array_merge($lang['blocks'], array(
 	'chat_title'	=> 'Chat',
 	'chat_explain'	=> 'Permite que você use cBB Chat como um bloco nas colunas',
+));
+
+$lang = array_merge($lang, array(
+	'CHAT_ADDON_MODE'		=> 'Modo de exibição',
+	'CHAT_MODE_CHAT'		=> 'Sala de chat',
+	'CHAT_MODE_USERS'		=> 'Usuários online',
 ));
